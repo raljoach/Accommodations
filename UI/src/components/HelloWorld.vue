aa121c37cc527508a5489278004b3f8f31568db7
Ralph Joachim <raljoach@gmail.com>
2021-04-16 22:11:49 -0700

    copy SearchAndPagination example start fixing vue compile errors

--- UI/src/components/HelloWorld.vue
+++ UI/src/components/HelloWorld.vue
@@ -1,40 +1,197 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <h3 class="title is-3">Vue v1 Search and Pagination</h3>
+  <h5 class="subtitle is-5">
+    credit : 
+    <a href="http://bulma.io/">bulma</a>, 
+    <a href="https://vuejs.org/">vuejs</a>, 
+    <a href="http://chancejs.com/">chancejs</a>, 
+    <a href="http://lodash.com/">lodash</a>
+  </h5>
+  <div class="box m-form">
+    <label class="label">Search Person Name</label>
+    <div class="control is-grouped">
+      <p class="control is-expanded">
+        <input class="input" v-model="searchItem" v-on:keyup="searchInTheList(searchItem)" type="text" placeholder="Find a person">
+        <span class="help is-dark"><strong>{{ numeral(filteredItems.length) }}</strong> of {{ numeral(items.length) }} person found</span>
+      </p>
+      <p class="control">
+        <a class="button is-info" v-on:click="clearSearchItem" v-bind:class="{'is-disabled': searchItem==''}">
+          Clear
+        </a>
+      </p>
+    </div>
+  </div>
+  <div class="box m-tags">
+    <span><strong>{{selectedItems.length}}</strong> person selected</span>
+    <div class="m-tags-items">
+      <a v-for="item in selectedItems" v-on:click="removeSelectedItem(item)" class="tag is-dark is-small">
+        {{item.name}}
+        <button class="delete is-small"></button>
+      </a>
+    </div>
+  </div>
+  <nav class="pagination m-pagination">
+    <a class="button" v-on:click="selectPage(this.pagination.currentPage-1)" v-bind:class="{'is-disabled': this.pagination.currentPage==this.pagination.items[0] || this.pagination.items.length==0}">Previous</a>
+    <a class="button" v-on:click="selectPage(this.pagination.currentPage+1)" v-bind:class="{'is-disabled': this.pagination.currentPage==this.pagination.items[this.pagination.items.length-1] || this.pagination.items.length==0}">Next page</a>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li>
+        <a class="button" v-on:click="selectPage(pagination.items[0])" v-bind:class="{'is-disabled': this.pagination.currentPage==this.pagination.items[0] || this.pagination.items.length==0}">
+          First
+        </a>
+      </li>
+      <li class="is-space"></li>
+      <li v-for="item in pagination.filteredItems">
+        <a class="button" v-on:click="selectPage(item)" v-bind:class="{'is-info': item == pagination.currentPage}">{{item | numeral}}</a>
+      </li>
+      <li class="is-space"></li>
+      <li>
+        <a class="button" v-on:click="selectPage(pagination.items[pagination.items.length-1])" v-bind:class="{'is-disabled': this.pagination.currentPage==this.pagination.items[this.pagination.items.length-1] || this.pagination.items.length==0}">
+          Last
+        </a>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  </nav>
+  <div class="m-table">
+    <table class="table is-bordered is-striped is-narrow">
+      <tr>
+        <th class="m-table-index">#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Phone</th>
+        <th></th>
+      </tr>
+      <tr v-for="item in paginatedItems">
+        <td>{{item.key}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.email}}</td>
+        <td>{{item.address}}</td>
+        <td>{{item.phone}}</td>
+        <td><a class="button is-info is-outlined" v-on:click="selectItem(item)">Select</a></td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+let items = []
+for(var i=0; i<20000; i++){
+  items.push({
+    key: i+1,
+    ssn: chance.ssn(),  // SSN as person ID
+    name: chance.name(),
+    email: chance.email(),
+    address: chance.address(),
+    phone: chance.phone(),
+    selected: false
+  })
+}
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: {
+    searchItem: '',
+    items: items,
+    filteredItems: [],
+    paginatedItems: [],
+    selectedItems: [],
+    pagination: {
+      range: 5,
+      currentPage: 1,
+      itemPerPage: 8,
+      items: [],
+      filteredItems: [],
+    }
+  },
+  ready() {    
+    this.filteredItems = this.items
+    this.buildPagination()
+    this.selectPage(1)    
+  },
+  methods: {
+    numeral(value){
+       return numeral(value).format('0,0');
+    },
+    clearSearchItem(){
+      this.searchItem = undefined
+      this.searchInTheList('')
+    },
+    searchInTheList(searchText, currentPage){
+      if(_.isUndefined(searchText)){
+        this.filteredItems = _.filter(this.items, function(v, k){
+          return !v.selected
+        })
+      }
+      else{
+        this.filteredItems = _.filter(this.items, function(v, k){
+          return !v.selected && v.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1
+        })
+      }
+      this.filteredItems.forEach(function(v, k){
+        v.key = k+1
+      })  
+      this.buildPagination()
+      
+      if(_.isUndefined(currentPage)){
+        this.selectPage(1) 
+      }
+      else{
+        this.selectPage(currentPage)
+      }
+    },
+    buildPagination(){
+      let numberOfPage = Math.ceil(this.filteredItems.length/this.pagination.itemPerPage)
+      this.pagination.items = []
+      for(var i=0; i<numberOfPage; i++){
+        this.pagination.items.push(i+1)
+      }
+    },
+    selectPage(item) {
+      this.pagination.currentPage = item
+      
+      let start = 0
+      let end = 0
+      if(this.pagination.currentPage < this.pagination.range-2){
+        start = 1
+        end = start+this.pagination.range-1
+      }
+      else if(this.pagination.currentPage <= this.pagination.items.length && this.pagination.currentPage > this.pagination.items.length - this.pagination.range + 2){
+        start = this.pagination.items.length-this.pagination.range+1
+        end = this.pagination.items.length
+      }
+      else{
+        start = this.pagination.currentPage-2
+        end = this.pagination.currentPage+2
+      }
+      if(start<1){
+        start = 1
+      }
+      if(end>this.pagination.items.length){
+        end = this.pagination.items.length
+      }
+      
+      this.pagination.filteredItems = []
+      for(var i=start; i<=end; i++){
+        this.pagination.filteredItems.push(i);
+      }
+      
+      this.paginatedItems = this.filteredItems.filter((v, k) => {
+        return Math.ceil((k+1) / this.pagination.itemPerPage) == this.pagination.currentPage
+      })
+    },
+    selectItem(item){
+      item.selected = true
+      this.selectedItems.push(item)
+      this.searchInTheList(this.searchItem, this.pagination.currentPage)
+    },    
+    removeSelectedItem(item){
+      item.selected = false
+      this.selectedItems.$remove(item)
+      this.searchInTheList(this.searchItem, this.pagination.currentPage)
+    }
   }
 }
 </script>
